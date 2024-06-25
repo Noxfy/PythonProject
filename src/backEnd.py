b48107e5770e2fdcf711613ef5459a3b90671c1b
Nolan Dacko <nolandacko@gmail.com>
2024-06-24 23:05:34 -0700

    fixxed player to work with vectors instead of lists

--- src/backEnd.py
+++ src/backEnd.py
@@ -11,13 +11,14 @@ class PlayerDirection(Enum):
 
 player_state = PlayerDirection.North
 player_moving = False
-player_position = [0, 0]
-player_speed = 0.01
-player_move_vec = [0, 0]
+player_position = pygame.Vector2(0, 0)
+player_speed = 0.05
+player_move_vec = pygame.Vector2(0, 0)
   
 def HandelEvents():
   global player_move_vec
   global player_moving
+  global player_position
   for event in pygame.event.get():
     
     if event.type == pygame.QUIT:
@@ -28,9 +29,7 @@ def HandelEvents():
 
   if (player_moving):
     #move player
-    player_position[0] += player_move_vec[0] * player_speed
-    player_position[1] -= player_move_vec[1] * player_speed
-
+    player_position += player_move_vec * player_speed
 
 def Render(screen):
   screen.fill((255, 255, 255))
@@ -49,23 +48,24 @@ def HandleMovement(event):
   keys = pygame.key.get_pressed()
   if event.type == pygame.KEYDOWN:
     if keys[pygame.K_w]:
-      player_move_vec[1] += 1
+      player_move_vec.y -= 1
       player_state = PlayerDirection.North
       player_moving = True
     if keys[pygame.K_d]:
-      player_move_vec[0] += 1
+      player_move_vec.x += 1
       player_state = PlayerDirection.East
       player_moving = True
     if keys[pygame.K_s]:
-      player_move_vec[1] -= 1
+      player_move_vec.y += 1
       player_state = PlayerDirection.South
       player_moving = True
     if keys[pygame.K_a]:
-      player_move_vec[0] -= 1
+      player_move_vec.x -= 1
       player_state = PlayerDirection.West
       player_moving = True
-    player_move_vec = util.normalize_vector(player_move_vec)
+    if player_move_vec.length() != 0:
+      player_move_vec = player_move_vec.normalize()
     
   if event.type == pygame.KEYUP and not (keys[pygame.K_w] or keys[pygame.K_a] or keys[pygame.K_s] or keys[pygame.K_d]):
     player_moving = False
-    player_move_vec = [0, 0]
+    player_move_vec = pygame.Vector2(0, 0)
--- src/util.py
+++ src/util.py
@@ -1,21 +1,11 @@
 import math, pygame
 
-def normalize_vector(vector):
-    x, y = vector
-    magnitude = math.sqrt(x**2 + y**2)
-    
-    if magnitude == 0:
-        raise ValueError("Cannot normalize a zero vector")
-    
-    normalized_vector = [x / magnitude, y / magnitude]
-    return normalized_vector
-
 def GetLookDirection(player_pos):
     mouse_pos = pygame.mouse.get_pos()
 
     vec2 = [None, None]
-    vec2[0] = mouse_pos[0] - player_pos[0]
-    vec2[1] = mouse_pos[1] - player_pos[1]
+    vec2[0] = mouse_pos[0] - player_pos.x
+    vec2[1] = mouse_pos[1] - player_pos.y
     vec1 = [0, 1]
 
     #angle_to_mouse = math.atan((mouse_pos[0] - player_pos[0]) / (mouse_pos[1] - player_pos[1])) * (180/math.pi)
